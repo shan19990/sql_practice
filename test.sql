51925e7128aff4b988f3d75dd19216df3e154e28
shan19990 <53429536+shan19990@users.noreply.github.com>
2023-11-20 18:47:50 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,8 @@
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+SELECT * FROM employees WHERE first_name = 'Kellie' AND gender = 'F'
