430b4f200b5ece81e3d172e03ae38910e9ddd83d
Shankhanil Ghosh <53429536+shan19990@users.noreply.github.com>
2023-11-24 22:45:35 +0530

    Add files via upload

--- Leet_Code_SQL_50.sql
+++ Leet_Code_SQL_50.sql
@@ -55,6 +55,10 @@ SUM(amount) AS trans_total_amount,
 IFNULL(SUM(CASE WHEN state = "approved" THEN amount END) ,0) AS approved_total_amount 
 FROM Transactions GROUP BY month,country;
 
+# Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+SELECT SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END)/COUNT(DISTINCT customer_id) * 100 AS immediate_percentage FROM Delivery WHERE (customer_id,order_date) IN 
+(SELECT customer_id, MIN(order_date) FROM Delivery GROUP BY customer_id);
+
 
 
 
