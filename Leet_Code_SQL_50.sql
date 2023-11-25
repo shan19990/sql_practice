614359b4ba0caffeeace94cfa2137bceae3adc5e
Shankhanil Ghosh <53429536+shan19990@users.noreply.github.com>
2023-11-25 12:50:01 +0530

    Add files via upload

--- Leet_Code_SQL_50.sql
+++ Leet_Code_SQL_50.sql
@@ -61,8 +61,35 @@ SELECT ROUND(SUM(CASE WHEN customer_pref_delivery_date = order_date THEN 1 ELSE 
 # Write a solution to calculate the number of unique subjects each teacher teaches in the university.
 SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt FROM Teacher GROUP BY teacher_id;
 
-# 
+# Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users FROM Activity WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27' GROUP BY activity_date;
 
+# Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+SELECT product_id, year AS first_year , quantity, price FROM Sales WHERE (product_id,year) IN (SELECT product_id, MIN(year) FROM Sales GROUP BY product_id);
+
+# Write a solution to find all the classes that have at least five students.
+SELECT class FROM Courses GROUP BY class HAVING Count(student) >= 5;
+
+# Write a solution that will, for each user, return the number of followers.
+SELECT user_id, COUNT(follower_id) AS followers_count FROM Followers GROUP BY user_id ORDER BY user_id;
+
+# Find the largest single number. If there is no single number, report null.
+SELECT MAX(num) AS num FROM MyNumbers WHERE num IN (
+SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1);
+
+# Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+SELECT customer_id  FROM Customer GROUP BY customer_id HAVING COUNT(DISTINCT product_key)/(SELECT COUNT(*) FROM Product) = 1;
+
+# Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+SELECT e1.employee_id,e1.name,COUNT(e2.employee_id) AS reports_count, ROUND(AVG(e2.age)) AS average_age FROM Employees e1 INNER JOIN Employees e2 ON e1.employee_id = e2.reports_to GROUP BY e1.employee_id ORDER BY e1.employee_id;
+
+# Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
+SELECT employee_id,department_id FROM Employee WHERE primary_flag = "Y"
+UNION
+SELECT employee_id,department_id FROM Employee GROUP BY employee_id HAVING COUNT(employee_id)=1;
+
+# Report for every three line segments whether they can form a triangle.
+SELECT x,y,z, CASE WHEN x+y>z AND x+z>y AND y+z>x THEN "Yes" ELSE "No" END AS triangle FROM Triangle;
 
 
 
